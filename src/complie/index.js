f41bd9ac7229ef7c22e783f689656f46ff14143b
yunhong.cai <276203988@qq.com>
2020-04-15 15:27:22 +0800

    初步完成endtag 文本解析

--- src/complie/index.js
+++ src/complie/index.js
@@ -1,4 +1,4 @@
-import {startTagOpen, stargTagClose, attribute} from './reg.js'
+import {startTagOpen, startTagClose, attribute, endTag} from './reg.js'
 
 function parse (html) {
     console.log(html)
@@ -8,7 +8,7 @@ function parse (html) {
 
     while(html){
         last = html
-        if(!lastTag){
+        if(!lastTag || !isPlainTextElement(lastTag)){
             var textEnd = html.indexOf('<')
             if(textEnd == 0){
                 // 如果<的位置是0的话，有很多种可能
@@ -19,12 +19,36 @@ function parse (html) {
 
                 //4. 元素节点，如<div></div>,其中又包含两种情况，
                 //可能是前半部分<div>,也可能是后半部份</div>
+
+                var endTagMatch = html.match(endTag)
+                if(endTagMatch){
+                    console.log(endTagMatch)
+                    advance(endTagMatch[0].length)
+                    continue
+                }
+
                 var startTagMatch = parseStartTag()
-                html = ''
+                if(startTagMatch){
+                    continue
+                }
+                
 
                 //5.文本节点，其中就包含{{message}}这种的
 
 
+            }
+            //
+            var text, rest, next;
+            if(textEnd >= 0){
+                rest = html.slice(textEnd)
+
+                text = html.slice(0, textEnd)
+            }
+            if(textEnd < 0){
+                text = html
+            }
+            if(text){
+                advance(text.length)
             }
         }
     }
@@ -39,33 +63,38 @@ function parse (html) {
                 attrs: [],
                 start: index
             }
-        }
-        advance(start[0].length)
-
-        //2. 解析 id='app'，实际情况是不只有id这个属性，可能有很多，因此有个while循环。
-        // 这里的逻辑是 一直要遇到 > 或者 />才结束。即整个startTag结束。
-        var end, attr;
-        while(!(end = html.match(stargTagClose)) && (attr = html.match(attribute))){
-            attr.start = index
-            advance(attr[0].length)
-            attr.end = index
-            match.attrs.push(attr)
-        }
-        // 解析 >， 也可能是 />。这里是>
-        if(end){
-            console.log(end)
-            match.unarySlash = end[1]
-            advance(end[0].length)
-            match.end = index
-            return match
-        }
         
+            advance(start[0].length)
+
+            //2. 解析 id='app'，实际情况是不只有id这个属性，可能有很多，因此有个while循环。
+            // 这里的逻辑是 一直要遇到 > 或者 />才结束。即整个startTag结束。
+            var end, attr;
+            while(!(end = html.match(startTagClose)) && (attr = html.match(attribute))){
+                attr.start = index
+                advance(attr[0].length)
+                attr.end = index
+                match.attrs.push(attr)
+            }
+            // 解析 >， 也可能是 />。这里是>
+            if(end){
+                console.log(end)
+                match.unarySlash = end[1]
+                advance(end[0].length)
+                match.end = index
+                return match
+            }
+        }
     }
 
     function advance (n) {
         index += n;
         html = html.substring(n);
       }
+}
+
+
+function isPlainTextElement() {
+    return false
 }
 
 
--- src/complie/reg.js
+++ src/complie/reg.js
@@ -3,22 +3,23 @@ var startTagOpen = /^<([a-zA-Z][\-\.a-zA-Z0-9]*)/
 
 // 匹配 >  有无^区别巨大
 // 这里捕获 斜杠 的原因是有可能是 <img /> <br/> 标签这种，而一般的，如div，是成对出现的
-var stargTagClose = /^\s*(\/?)>/
+var startTagClose = /^\s*(\/?)>/
 
-// 这里直接抄自源码
-//匹配 id='app'（后面都以这个举例），需要捕获的实际上 是 id, 等号，app。
-//带有？：是不捕获的分组
-
-/*
+/* 
+这里直接抄自源码
+匹配 id='app'（后面都以这个举例），需要捕获的实际上 是 id, 等号，app。
+带有？：是不捕获的分组
 之所以这么长，实际上是兼容了有可能存在的很多写法:
 空格 比如 id   ='app'等等
 分号是双引号还是单引号 id='app'  或者 id="app"
 */
 var attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/;
 
+var endTag = /^<\/[a-zA-Z][\-\.a-zA-Z0-9]*>/
 
 export {
     startTagOpen,
-    stargTagClose,
-    attribute
+    startTagClose,
+    attribute,
+    endTag
 }
